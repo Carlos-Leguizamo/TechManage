2e8850700b770ecfe7158fb802fc80be5b941787
Carlos Leguizamo <tato2005leguizamo@gamil.com>
2024-09-02 14:27:27 -0500

    Tabla de administracion de usurios realizada y decorador aplicado

--- users/views.py
+++ users/views.py
@@ -1,8 +1,15 @@
-# views/urls.py
-
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required 
 
-
-# Create your views here.
+@login_required
 def adminusers(request):
-    return render(request, "admin-users.html")
+    # User.objects.all traigo todo los usuarios de la BD
+    users_list = User.objects.all()
+    paginator = Paginator(users_list, 5)  # Muestra 10 usuarios por página
+
+    page_number = request.GET.get('page')
+    users = paginator.get_page(page_number)
+
+    return render(request, 'admin-users.html', {'users': users})
