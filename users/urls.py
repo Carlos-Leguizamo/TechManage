0a9d26d24320dbace9e73843078828f077bd2a94
Carlos Leguizamo <tato2005leguizamo@gamil.com>
2024-09-02 17:22:54 -0500

    Delete de Users - Se pasa ID por URL

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,9 @@ from . import views
 
 urlpatterns = [
     path("admin-user/", views.adminusers, name='adminusers'),
+    # path('user_management/', views.user_management, name='user_management'),  # Añade esta línea
+    path('delete_user/<int:user_id>/', views.confirm_delete_user, name='confirm_delete_user'),
+#  path('confirm-delete/', views.confirm_delete_user, name='confirm_delete_user'),
    
     
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,33 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 
+
+# Logica para mostar los usuarios en la tabal y retonrar la vista de la tabla de administracion de usuarios
 @login_required
 def adminusers(request):
     # User.objects.all traigo todo los usuarios de la BD
     users_list = User.objects.all()
     paginator = Paginator(users_list, 5)  # Muestra 10 usuarios por página
 
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     users = paginator.get_page(page_number)
 
-    return render(request, 'admin-users.html', {'users': users})
+    return render(request, "admin-users.html", {"users": users})
+
+
+@login_required
+def confirm_delete_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+
+    if request.method == "POST":
+        user.delete()
+        messages.success(request, "Usuario eliminado con éxito.")
+        return redirect(
+            "adminusers"
+        )  # Redirige a la página de administración de usuarios
+
+    return render(request, "confirm_delete_user.html", {"user": user})
