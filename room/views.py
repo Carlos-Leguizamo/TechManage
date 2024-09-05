b0a09e5c6ec54182dc482dfae937fb63546fe57a
Santiago_Firigua <santiagofiriguapalma@gmail.com>
2024-09-05 10:13:17 -0500

    nuevos estilos css para sala.html y para pc.html

--- room/views.py
+++ room/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Sala, Computadores
 from .forms import SalaForm, ComputadorForm
 
