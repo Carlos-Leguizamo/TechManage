e30027cb6013e7b001f9351f4a97e58f39ca273c
Carlos Leguizamo <tato2005leguizamo@gamil.com>
2024-09-06 13:49:55 -0500

    Front de Salas y PC

--- dashboard/models.py
+++ dashboard/models.py
@@ -17,64 +17,6 @@ class PQRS(models.Model):
     class Meta:
         db_table = 'PQRS'
 
-class Sala(models.Model):
-    id_sala = models.AutoField(primary_key=True)
-    nombre_sala = models.CharField(max_length=45)
-    descripcion = models.TextField()
-    capacidad = models.IntegerField()
-    ubicacion = models.CharField(max_length=45)
-    fecha_creacion = models.DateTimeField()
-    fecha_actualizacion = models.DateTimeField()
-    estado = models.BooleanField()  # Usamos BooleanField para TINYINT (0 o 1)
-    inventario_cantidad = models.IntegerField()
-
-    class Meta:
-        db_table = 'Sala'
-
-class Computadores(models.Model):
-    ESTADO_CHOICES = [
-        ('operativo', 'Operativo'),
-        ('en reparación', 'En reparación'),
-        ('dado de baja', 'Dado de baja'),
-    ]
-
-    id_computador = models.AutoField(primary_key=True)
-    tipo = models.CharField(max_length=45)
-    marca = models.CharField(max_length=45)
-    modelo = models.CharField(max_length=45)
-    numero_serie = models.CharField(max_length=45)
-    fecha_adquisicion = models.DateTimeField()
-    estado = models.CharField(max_length=20, choices=ESTADO_CHOICES)
-    id_sala = models.ForeignKey(Sala, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'Computadores'
-
-class Perifericos(models.Model):
-    TIPO_CHOICES = [
-        ('mouse', 'Mouse'),
-        ('teclado', 'Teclado'),
-        ('monitor', 'Monitor'),
-        ('camara', 'Cámara'),
-    ]
-    
-    ESTADO_CHOICES = [
-        ('operativo', 'Operativo'),
-        ('en reparación', 'En reparación'),
-        ('dado de baja', 'Dado de baja'),
-    ]
-
-    id_perifericos = models.AutoField(primary_key=True)
-    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
-    marca = models.CharField(max_length=45)
-    modelo = models.CharField(max_length=45)
-    estado = models.CharField(max_length=20, choices=ESTADO_CHOICES)
-    descripcion = models.TextField()
-    id_computador = models.ForeignKey(Computadores, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'Perifericos'
-
 class Reportes(models.Model):
     id_reportes = models.AutoField(primary_key=True)
     nombre_reporte = models.CharField(max_length=45)
@@ -83,23 +25,4 @@ class Reportes(models.Model):
     id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'Reportes'
-
-class Mantenimiento(models.Model):
-    TIPO_MANTENIMIENTO_CHOICES = [
-        ('preventivo', 'Preventivo'),
-        ('correctivo', 'Correctivo'),
-    ]
-
-    id_mantenimiento = models.AutoField(primary_key=True)
-    id_computador = models.ForeignKey(Computadores, on_delete=models.CASCADE)
-    fecha_programada = models.DateTimeField()
-    fecha_realizacion = models.DateTimeField()
-    tipo_mantenimiento = models.CharField(max_length=20, choices=TIPO_MANTENIMIENTO_CHOICES)
-    descripcion_mantenimiento = models.TextField()
-    tecnico_responsable = models.CharField(max_length=45)
-    proximo_mantenimiento = models.DateTimeField()
-    id = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'Mantenimiento'
+        db_table='Reportes'
