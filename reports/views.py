5dd56950219837bfe79c27651bd5458db9178b6b
Santiago_Firigua <santiagofiriguapalma@gmail.com>
2024-09-15 16:57:27 -0500

    funcion de pdf para reportes

--- reports/views.py
+++ reports/views.py
@@ -3,6 +3,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Reportes
 from .forms import ReporteForm
 from room.models import Computadores
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 def reports(request):
     reportes_list = Reportes.objects.all()
@@ -27,19 +30,21 @@ def new_report(request):
         form = ReporteForm()
     return render(request, "new_reports.html", {'form': form, 'computadores': computadores})
 
-def edit_report(request, id_reportes):
+# View para convertir reporte a PDF
+def generate_pdf_view(request, id_reportes):
     reporte = get_object_or_404(Reportes, id_reportes=id_reportes)
-    computadores = Computadores.objects.all()
-    if request.method == 'POST':
-        form = ReporteForm(request.POST, instance=reporte)
-        if form.is_valid():
-            form.save()
-            return redirect('reports')
-    else:
-        form = ReporteForm(instance=reporte)
-    return render(request, 'new_reports.html', {'form': form, 'computadores': computadores})
-
-def delete_report(request, id_reportes):
-    reporte = get_object_or_404(Reportes, id_reportes=id_reportes)
-    reporte.delete()
-    return redirect('reports')
+    template_path = 'pdf_template.html'
+    context = {'reporte': reporte}
+    
+    # Cargar plantilla y contexto en el PDF
+    template = get_template(template_path)
+    html = template.render(context)
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="reporte.pdf"'
+    
+    # Generar PDF usando pisa
+    pisa_status = pisa.CreatePDF(html, dest=response)
+    if pisa_status.err:
+        return HttpResponse('Error al generar el PDF: %s' % pisa_status.err)
+    
+    return response
