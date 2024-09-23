04afd789e539654c7eba982707ddfe92aa95f6b0
Carlos Leguizamo <tato2005leguizamo@gamil.com>
2024-09-23 10:04:41 -0500

    URLS PQRS

--- pqrs/urls.py
+++ pqrs/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
        path("pending-pqrs/", views.pending_pqrs, name='view-pending-pqrs'), # Vista de PQRS pendientes del usuario
        path("admin-pqrs/", views.admin_pqrs, name='view-admin-pqrs'),
        path('confirm-delete-pqrs/<int:id>/', views.confirm_delete_pqrs, name='confirm_delete_pqrs'),
-
-       
        path('check-pqrs/<int:id>/', views.check_pqrs, name='check_pqrs'),
 
 
