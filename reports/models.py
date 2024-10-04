0def8d6a34dbbbd388e6152c99886f6df823b3c2
Santiago_Firigua <santiagofiriguapalma@gmail.com>
2024-10-04 13:44:18 -0500

    crear campo tipo_mantenimiento para reportes

--- reports/models.py
+++ reports/models.py
@@ -4,13 +4,18 @@ from room.models import Computadores
 from django.utils import timezone
 
 class Reportes(models.Model):
+    TIPO_MANTENIMIENTO_CHOICES = [
+        ('Preventivo', 'Preventivo'),
+        ('Correctivo', 'Correctivo'),
+    ]
+
     id_reportes = models.AutoField(primary_key=True)
     nombre_reporte = models.CharField(max_length=45)
     fecha = models.DateTimeField(default=timezone.now) 
     descripcion = models.TextField()
     id_computador = models.ForeignKey(Computadores, on_delete=models.CASCADE, db_column='id_computador')
     tecnico_responsable = models.CharField(max_length=45)
+    tipo_mantenimiento = models.CharField(max_length=11, choices=TIPO_MANTENIMIENTO_CHOICES, default='Preventivo')
 
     class Meta:
         db_table='Reportes'
- 
